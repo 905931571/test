a785061a4a4b79270b964543a3a162cdab4d8953
lzm <905931571@qq.com>
2019-08-27 19:42:40 +0800

    152

--- src/main/java/com/tedu/sp06/consoller/RibbonController.java
+++ src/main/java/com/tedu/sp06/consoller/RibbonController.java
@@ -31,7 +31,7 @@ public class RibbonController {
 		return rt.postForObject("http://item-service/decreaseNumber", items, JsonResult.class);
 	}
 
-	/////////////////////////////////////////
+	/////////////////////////////////////////11111
 	
 	@GetMapping("/user-service/{userId}")
 	public JsonResult<User> getUser(@PathVariable Integer userId) {
